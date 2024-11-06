9b85e595c88721503ea4370ed1518b0de2f83fe6
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-11-06 16:48:42 +0100

    initial commit

---
+++ projects/shared-layout/src/lib/shared-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-layout',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      shared-layout works!
+      
+    </p>
+  `,
+  styles: ``
+})
+export class SharedLayoutComponent {
+
+}
---
+++ projects/shared-layout/src/public-api.ts
@@ -0,0 +1,13 @@
+/*
+ * Public API Surface of shared-layout
+ */
+
+export * from './lib/shared-layout.service';
+export * from './lib/shared-layout.component';
+
+// projects/shared-layout/src/public-api.ts
+export * from './lib/components/header/header.component';
+export * from './lib/components/footer/footer.component';
+export * from './lib/components/sidebar/sidebar.component';
+// export * from './lib/themes/theme-dark.scss';
+// export * from './lib/themes/theme-dark.scss';
